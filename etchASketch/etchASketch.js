a2ae84214813928856c3886e8e35f071096c9ecb
Tom Fishwick <fishwickt@gmail.com>
2021-03-16 21:51:03 +0000

    Odin Etch a sketch

---
+++ etchASketch/etchASketch.js
@@ -0,0 +1,36 @@
+const $sketchpad = $("#sketchpad");
+let gridnum = 16;
+
+$(window).on("load",() => {
+    createGrid();
+});
+
+const createGrid = () => {
+    for (let i = 0; i < gridnum; i++) {
+        $sketchpad.append("<div id='row" + i.toString() + "' class='row'></div>");
+        let $row = $("#row" + i.toString());
+        for (let j = 0; j < gridnum; j++) {
+            $row.append('<div id="coli' + i.toString() + 'j' + j.toString() + '" class="col"></div>');
+        }
+    }
+
+    $('.col').hover((obj) => {
+        //console.log('test', obj.target.id);
+        $('#' + obj.target.id).addClass('hovered');
+    });
+    $('.col').css({
+        "width": (900 / gridnum).toString() + 'px',
+        'height': (900 / gridnum).toString() + 'px'
+    });
+};
+
+const userCreateGrid = () => {
+    console.log("test");
+    gridnum = Number(prompt("Select size of grid (max 100)", gridnum));
+    if (gridnum < 1 || gridnum > 100 || !gridnum) {
+        gridnum = 100;
+    }
+    console.log('gridnum', gridnum);
+    $sketchpad.empty();
+    createGrid();
+}
