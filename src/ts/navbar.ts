f9f14581ee9a29bfe5f6c8de1ee7332af45d8dd2
Tom Fishwick <fishwickt@gmail.com>
2022-08-03 19:41:08 +0100

    app privacy policy

--- src/ts/navbar.ts
+++ src/ts/navbar.ts
@@ -9,7 +9,7 @@ const NAVHTML : string = `
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="pull-left" href="index.html"><img src="./images/link477.png" alt="Link477" style="height: 50px; width: 50px; background-color: white;"/></a>
+        <a class="pull-left" href="index.html"><img src="https://link477.com/images/link477.png" alt="Link477" style="height: 50px; width: 50px; background-color: white;"/></a>
     </div>
     <div class="collapse navbar-collapse">
         <ul class="nav navbar-nav">                           
@@ -18,34 +18,34 @@ const NAVHTML : string = `
                 <span class="caret"></span></a>
                 <ul class="dropdown-menu">
                     <li class="dropdown-header">Responsive Web Design</li>
-                    <li><a href="./fccResponsiveWebDesign/tributePage.html">Tribute Page</a></li>
-                    <li><a href="./fccResponsiveWebDesign/surveyForm.html">Survey Form</a></li>
-                    <li><a href="./fccResponsiveWebDesign/productLandingPage.html">Product Landing Page</a></li>
-                    <li><a href="./fccResponsiveWebDesign/technicalDocPage.html">Technical Documentation Page</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/tributePage.html">Tribute Page</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/surveyForm.html">Survey Form</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/productLandingPage.html">Product Landing Page</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/technicalDocPage.html">Technical Documentation Page</a></li>
                     <li class="dropdown-header">JavaScript Algorithms and Data Structures</li>
-                    <li><a href="./fccResponsiveWebDesign/fccJs.html">All five projects on this page</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/fccJs.html">All five projects on this page</a></li>
                     <li class="dropdown-header">Front End Libraries Projects</li>
-                    <li><a href="./fccResponsiveWebDesign/randomQuoteMachine.html">Random Quote Machine</a></li>
-                    <li><a href="./fccResponsiveWebDesign/markdownPreviewer.html">Markdown Previewer</a></li>
-                    <li><a href="./fccResponsiveWebDesign/drumMachine.html">Drum Machine</a></li>
-                    <li><a href="./fccResponsiveWebDesign/calculator.html">Javascript Calculator</a></li>
-                    <li><a href="./fccResponsiveWebDesign/pomodoroClock.html">25 + 5 Clock</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/randomQuoteMachine.html">Random Quote Machine</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/markdownPreviewer.html">Markdown Previewer</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/drumMachine.html">Drum Machine</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/calculator.html">Javascript Calculator</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/pomodoroClock.html">25 + 5 Clock</a></li>
                     <li class="dropdown-header">Data Visualisation Projects</li>
-                    <li><a href="./fccResponsiveWebDesign/barChart.html">Bar Chart</a></li>
-                    <li><a href="./fccResponsiveWebDesign/scatterplot.html">Scatterplot Graph</a></li>
-                    <li><a href="./fccResponsiveWebDesign/heatmap.html">Heat Map</a></li>
-                    <li><a href="./fccResponsiveWebDesign/choropleth.html">Choropleth Map</a></li>
-                    <li><a href="./fccResponsiveWebDesign/treemap.html">Treemap Diagram</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/barChart.html">Bar Chart</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/scatterplot.html">Scatterplot Graph</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/heatmap.html">Heat Map</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/choropleth.html">Choropleth Map</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/treemap.html">Treemap Diagram</a></li>
                     <li class="dropdown-header">Take Home Projects</li>
-                    <li><a href="./weather/index.html">Weather</a></li>
-                    <li><a href="./TicTacToe/index.html">Tic Tac Toe</a></li>
-                    <li><a href="./recipeBox/index.html">Recipe Box</a></li>
-                    <li><a href="./PongGame/index.html">Pong Game</a></li>
-                    <li><a href="./simonGame/index.html">Simon Game</a></li>
-                    <li><a href="./fccResponsiveWebDesign/fccForum.html">Free Code Camp Forum Homepage</a></li>
-                    <li><a href="./gameOfLife/index.html">Game of Life</a></li>
+                    <li><a href="https://link477.com/weather/index.html">Weather</a></li>
+                    <li><a href="https://link477.com/TicTacToe/index.html">Tic Tac Toe</a></li>
+                    <li><a href="https://link477.com/recipeBox/index.html">Recipe Box</a></li>
+                    <li><a href="https://link477.com/PongGame/index.html">Pong Game</a></li>
+                    <li><a href="https://link477.com/simonGame/index.html">Simon Game</a></li>
+                    <li><a href="https://link477.com/fccResponsiveWebDesign/fccForum.html">Free Code Camp Forum Homepage</a></li>
+                    <li><a href="https://link477.com/gameOfLife/index.html">Game of Life</a></li>
                     <li class="dropdown-header">Other Projects</li>
-                    <li><a href="./JSPianoKeyboard/index.html">JavaScript Keyboard</a></li>
+                    <li><a href="https://link477.com/JSPianoKeyboard/index.html">JavaScript Keyboard</a></li>
                 </ul>
             </li>
             
@@ -54,9 +54,9 @@ const NAVHTML : string = `
                 <span class="caret"></span></a>
                 <ul class="dropdown-menu">
                     <li class="dropdown-header">Project Odin</li>
-                    <li><a href="./etchASketch/index.html">Etch A Sketch</a></li>
-                    <li><a href="./rockPaperScissors.html">Rock, Paper, Scissors</a></li>
-                    <li><a href="./battleship/index.html">Battleship</a></li>
+                    <li><a href="https://link477.com/etchASketch/index.html">Etch A Sketch</a></li>
+                    <li><a href="https://link477.com/rockPaperScissors.html">Rock, Paper, Scissors</a></li>
+                    <li><a href="https://link477.com/battleship/index.html">Battleship</a></li>
                 </ul>
             </li>
 
@@ -65,8 +65,8 @@ const NAVHTML : string = `
                 <span class="caret"></span></a>
                 <ul class="dropdown-menu">
                     <li class="dropdown-header">Games</li>
-                    <li><a href="./ColorGridGame/Index.html">Colour Grid</a></li>
-                    <li><a href="./Snake/index.html">Snake</a></li>
+                    <li><a href="https://link477.com/ColorGridGame/Index.html">Colour Grid</a></li>
+                    <li><a href="https://link477.com/Snake/index.html">Snake</a></li>
                 </ul>
             </li>
             <li><a href="modelrailway.html">Model Railway</a></li>
