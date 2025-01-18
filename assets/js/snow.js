1e094e26abe2557df016b74f147ce9656caead5e
Tom Fishwick <51207735+SL477@users.noreply.github.com>
2025-01-18 18:27:27 +0000

    initial effect

--- assets/js/snow.js
+++ assets/js/snow.js
@@ -4,23 +4,23 @@
 function snow() {
   const main = document.body;
   const snowHolder = document.createElement('div');
-  snowHolder.style.height = '100%';
-  snowHolder.style.width = '100vw';
-  snowHolder.style.margin = 0;
-  snowHolder.style.display = 'flex';
-  snowHolder.style.position = 'absolute';
+  // snowHolder.style.height = '100%';
+  // snowHolder.style.width = '100vw';
+  // snowHolder.style.margin = 0;
+  // snowHolder.style.display = 'flex';
+  // snowHolder.style.position = 'absolute';
+  snowHolder.ariaHidden = true;
   for (let i = 0; i < 100; i++) {
-    const marquee = document.createElement('marquee');
-    marquee.direction = 'down';
-    marquee.scrollDelay = Math.random() * 100 + 50;
-    marquee.style.color = 'white';
-    marquee.textContent = '*';
-    snowHolder.appendChild(marquee);
+    const flake = document.createElement('div');
+    flake.style.color = 'white';
+    flake.textContent = '❅';
+    flake.className = 'snowflake snowflakeInner';
+    snowHolder.appendChild(flake);
   }
   main.appendChild(snowHolder);
 }
 
 const now = new Date();
-if (now.getMonth() == 11) {
-  snow();
-}
+//if (now.getMonth() == 11) {
+snow();
+//}
