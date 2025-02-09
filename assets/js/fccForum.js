c1a10e19a4961e6ab283b575922cb6e59eb81eea
Tom Fishwick <fishwickt@gmail.com>
2025-02-09 16:11:12 +0000

    Update first batch of FCC stuff to new UI

--- assets/js/fccForum.js
+++ assets/js/fccForum.js
@@ -2,75 +2,76 @@
 let topic_list = [];
 let users = [];
 fetch('https://forum-proxy.freecodecamp.rocks/latest')
-    .then(response => response.json())
-    .then(data => {
-        topic_list = data.topic_list;
-        users = data.users;
-    })
-    .then(() => {
-        const topicsTBody = document.getElementById('topics');
-        if (topicsTBody) {
-            topic_list.topics.forEach(topic => {
+  .then((response) => response.json())
+  .then((data) => {
+    topic_list = data.topic_list;
+    users = data.users;
+  })
+  .then(() => {
+    const topicsTBody = document.getElementById('topics');
+    if (topicsTBody) {
+      topic_list.topics.forEach((topic) => {
+        const topicRow = document.createElement('tr');
+        const topicLinkTd = document.createElement('td');
+        const topicLinkA = document.createElement('a');
+        topicLinkA.href = `https://forum.freecodecamp.org/t/${topic.slug}/${topic.id}`;
+        topicLinkA.textContent = topic.id;
+        topicLinkTd.appendChild(topicLinkA);
+        topicRow.appendChild(topicLinkTd);
 
-                const topicRow = document.createElement('tr');
-                const topicLinkTd = document.createElement('td');
-                const topicLinkA = document.createElement('a');
-                topicLinkA.href = `https://forum.freecodecamp.org/t/${topic.slug}/${topic.id}`;
-                topicLinkA.textContent = topic.id;
-                topicLinkTd.appendChild(topicLinkA);
-                topicRow.appendChild(topicLinkTd);
+        const topicTitle = document.createElement('td');
+        topicTitle.textContent = topic.title;
+        topicRow.appendChild(topicTitle);
 
-                const topicTitle = document.createElement('td');
-                topicTitle.textContent = topic.title;
-                topicRow.appendChild(topicTitle);
+        // get the users
+        const topicUsers = document.createElement('td');
+        topic.posters.forEach((user) => {
+          const usrIndex = users.findIndex((u) => u.id == user.user_id);
+          if (usrIndex > -1) {
+            const userLink = document.createElement('a');
+            userLink.href = `https://forum.freecodecamp.org/u/${users[usrIndex].username}`;
 
-                // get the users
-                const topicUsers = document.createElement('td');
-                topic.posters.forEach(user => {
-                    const usrIndex = users.findIndex(u => u.id == user.user_id);
-                    if (usrIndex > -1) {
-                        const userLink = document.createElement('a');
-                        userLink.href = `https://forum.freecodecamp.org/u/${users[usrIndex].username}`;
+            const usrImg = document.createElement('img');
+            usrImg.className = 'usrImg';
+            let lnk = users[usrIndex].avatar_template;
 
-                        const usrImg = document.createElement('img');
-                        usrImg.className = 'usrImg';
-                        let lnk = users[usrIndex].avatar_template;
+            if (lnk.startsWith('/')) {
+              lnk = 'https://sjc1.discourse-cdn.com/freecodecamp' + lnk;
+            }
+            lnk = lnk.replace('{size}', '120');
+            usrImg.src = lnk;
+            usrImg.alt = users[usrIndex].username;
+            userLink.appendChild(usrImg);
 
-                        if (lnk.startsWith('/')) {
-                            lnk = 'https://sjc1.discourse-cdn.com/freecodecamp' + lnk;
-                        }
-                        lnk = lnk.replace('{size}','120');
-                        usrImg.src = lnk;
-                        usrImg.alt = users[usrIndex].username;
-                        userLink.appendChild(usrImg);
+            topicUsers.appendChild(userLink);
+          }
+        });
+        topicRow.appendChild(topicUsers);
 
-                        topicUsers.appendChild(userLink);
-                    }
-                });
-                topicRow.appendChild(topicUsers);
+        // reply count
+        const topicReplyCountTd = document.createElement('td');
+        topicReplyCountTd.textContent = topic.reply_count;
+        topicRow.appendChild(topicReplyCountTd);
 
-                // reply count
-                const topicReplyCountTd = document.createElement('td');
-                topicReplyCountTd.textContent = topic.reply_count;
-                topicRow.appendChild(topicReplyCountTd);
+        // views
+        const topicViews = document.createElement('td');
+        topicViews.textContent = topic.views;
+        topicRow.appendChild(topicViews);
 
-                // views
-                const topicViews = document.createElement('td');
-                topicViews.textContent = topic.views;
-                topicRow.appendChild(topicViews);
+        // last posted
+        const topicLastPostedTd = document.createElement('td');
+        const topicLastPostedTime = document.createElement('time');
+        topicLastPostedTime.dateTime = topic.last_posted_at;
+        topicLastPostedTime.textContent = new Date(
+          topic.last_posted_at
+        ).toLocaleDateString();
+        topicLastPostedTd.appendChild(topicLastPostedTime);
+        topicRow.appendChild(topicLastPostedTd);
 
-                // last posted
-                const topicLastPostedTd = document.createElement('td');
-                const topicLastPostedTime = document.createElement('time');
-                topicLastPostedTime.dateTime = topic.last_posted_at;
-                topicLastPostedTime.textContent = topic.last_posted_at;
-                topicLastPostedTd.appendChild(topicLastPostedTime);
-                topicRow.appendChild(topicLastPostedTd);
-
-                topicsTBody.appendChild(topicRow);
-            });
-        }
-    })
-    .catch(ex => {
-        console.error(ex);
-    });
+        topicsTBody.appendChild(topicRow);
+      });
+    }
+  })
+  .catch((ex) => {
+    console.error(ex);
+  });
