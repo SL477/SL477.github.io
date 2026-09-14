321a4c4465b4d75a7e03eeaa9ac26b7788fd50d2
Tom Fishwick <fishwickt@gmail.com>
2026-09-14 18:57:05 +0100

    Created sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import otherLinks from './src/content/otherLinks.json';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://link477.com/",
+  site: "https://link477.com/",
+  integrations: [sitemap({
+    customPages: otherLinks
+  })],
 });
